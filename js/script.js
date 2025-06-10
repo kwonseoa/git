98f76b0ac3de9879c2aef9d15c6547502bcb2670
권서아 <kms89133@naver.com>
2025-06-10 21:55:24 +0900

    포트폴리오 2차 업데이트

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(function () {
+  $(document).on("click", 'a[href="#"]', function (e) {
+    e.preventDefault();
+  });
+
   // gnb 스플리팅 호출
   Splitting();
 
@@ -65,6 +69,11 @@ $(function () {
         start: "0% 100%",
         end: "0% 20%",
         scrub: 1,
+        // onLeave: () => {
+        //   gsap.set(".con02 .title .txt_a, .con02 .title .txt_b", {
+        //     display: "none",
+        //   });
+        // },
       },
     })
     .fromTo(
@@ -124,10 +133,44 @@ $(function () {
         start: "100% 50%",
         end: "100% 0%",
         scrub: 1,
+        markers: true,
       },
     })
     .to(".con02 .title .txt_a", { x: "-100%", ease: "none", duration: 5 }, 0)
     .to(".con02 .title .txt_b", { x: "100%", ease: "none", duration: 5 }, 0);
+
+  // .con02와 .work_list 구간에서 title 글자 display 관리
+  ScrollTrigger.create({
+    trigger: ".con02", // 이 요소가 스크롤 트리거 시작점의 기준이 돼요
+    start: "top bottom", // .con02의 맨 위가 화면 맨 아래에 닿을 때 시작!
+    endTrigger: ".work_list", // 이 요소가 스크롤 트리거 끝점의 기준이 돼요
+    end: "bottom top", // .work_list의 맨 아래가 화면 맨 위에 닿을 때 끝!
+
+    onEnter: () => {
+      // 스크롤을 내려서 트리거 구간에 진입할 때
+      gsap.set(".con02 .title .txt_a, .con02 .title .txt_b", {
+        display: "block",
+      });
+    },
+    onLeave: () => {
+      // 스크롤을 내려서 트리거 구간을 벗어날 때
+      gsap.set(".con02 .title .txt_a, .con02 .title .txt_b", {
+        display: "none",
+      });
+    },
+    onEnterBack: () => {
+      // 스크롤을 올려서 트리거 구간에 다시 진입할 때
+      gsap.set(".con02 .title .txt_a, .con02 .title .txt_b", {
+        display: "block",
+      });
+    },
+    onLeaveBack: () => {
+      // 스크롤을 올려서 트리거 구간을 완전히 벗어날 때 (맨 위로 갈 때)
+      gsap.set(".con02 .title .txt_a, .con02 .title .txt_b", {
+        display: "none",
+      });
+    },
+  });
 
   // skill list img 무한루프
   const $list = $(".loop_list .list");
@@ -139,14 +182,68 @@ $(function () {
         trigger: ".con03",
         start: "0% 50%",
         end: "20% 50%",
-        markers: true,
+        // markers: true,
         // scrub: 1,
       },
     })
     .to(".loop_list", {
-      transform: "rotate(5deg) translate(0%, 400px) scale(1.1)",
+      transform: "rotate(5deg) translate(5%, 400px) scale(1.1)",
       opacity: "1",
       ease: "none",
       // duration: 5,
     });
+
+  // con04
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".content_wrap",
+        start: "0% 0%",
+        end: "100% 0%",
+        scrub: 1,
+        pin: true,
+        markers: true,
+      },
+    })
+    .to(".content_wrap h1", { opacity: "1", ease: "none", duration: "10" }, 5)
+    .to(
+      ".content_wrap img",
+      { scale: "0.4", ease: "none", duration: "10", opacity: "0.3" },
+      5
+    );
+
+  gsap.utils.toArray(".con04 .obj").forEach(function (obj) {
+    //toArray() => 배열로 만드는 메소드
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: obj,
+        start: "50% 100%",
+        toggleClass: { targets: obj, className: "active" },
+        scrub: 1,
+      },
+    });
+  });
+
+  gsap.utils.toArray(".con04 .text_box").forEach(function (txt) {
+    //toArray() => 배열로 만드는 메소드
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: txt,
+        start: "50% 80%",
+        end: "100% 0%",
+        toggleClass: { targets: txt, className: "active" },
+        scrub: 1,
+        markers: true,
+      },
+    });
+  });
+
+  // menu_open
+  $(".menu_open").on("click", function () {
+    $(".gnb").toggleClass("on");
+    $(this).toggleClass("on");
+    $("body").toggleClass("on"); // 스크롤 방지
+  });
 });
